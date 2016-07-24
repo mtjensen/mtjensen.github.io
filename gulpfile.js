a061a4363c68a155dacce15786451451d581c08f
mtjensen <mtyljensen@gmail.com>
2016-07-24 14:58:26 +0200

    images updated

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp')
+
+gulp.task('vendor', function() {
+	gulp.src('./node_modules/animsition/dist/**/*')
+		.pipe(gulp.dest('./vendor/animsition'))
+	gulp.src('./node_modules/fullpage.js/**/*')
+		.pipe(gulp.dest('./vendor/fullpage.js'))
+	gulp.src('./node_modules/fittext.js/jquery.fittext.js')
+		.pipe(gulp.dest('./vendor/fittext.js'))
+});
