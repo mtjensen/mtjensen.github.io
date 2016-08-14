919e89919a9dfa39da8d83532735cc9d9e2fba7f
mtjensen <mtyljensen@gmail.com>
2016-08-14 20:48:21 +0200

    added javascript compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
 
-var $NODE = "./node_modules"
+var nodePath = "./node_modules";
 
-gulp.task('vendor', function() {
-	gulp.src(`${$NODE}/animsition/dist/**/*`)
-		.pipe(gulp.dest('./vendor/animsition'))
-	gulp.src(`${$NODE}/fullpage.js/**/*`)
-		.pipe(gulp.dest('./vendor/fullpage.js'))
-	gulp.src(`${$NODE}/fittext.js/jquery.fittext.js`)
-		.pipe(gulp.dest('./vendor/fittext.js'))
-	gulp.src(`${$NODE}/mfb/src/**/*`)
-		.pipe(gulp.dest('./vendor/mfb'))
-});
+// gulp.task('vendor', function() {
+// 	gulp.src(`${nodePath}/animsition/dist/**/*`)
+// 		.pipe(gulp.dest('./vendor/animsition'))
+// 	gulp.src(`${nodePath}/fullpage.js/**/*`)
+// 		.pipe(gulp.dest('./vendor/fullpage.js'))
+// 	gulp.src(`${nodePath}/fittext.js/jquery.fittext.js`)
+// 		.pipe(gulp.dest('./vendor/fittext.js'))
+// 	gulp.src(`${nodePath}/mfb/src/**/*`)
+// 		.pipe(gulp.dest('./vendor/mfb'))
+// });
+
+var javascriptSources = [
+	`${nodePath}/bootstrap/dist/js/bootstrap.js`, 
+	`${nodePath}/fullpage.js/jquery.fullPage.js`, 
+	`${nodePath}/animsition/dist/js/animsition.js`, 
+	`${nodePath}/fittext.js/jquery.fittext.js`, 
+	`./src/classie/classie.js`,
+	`./src/cbpAnimatedHeader/cbpAnimatedHeader.js`, 
+	`${nodePath}/mfb/src/mfb.js`,
+	`./src/main.js`,
+	`./src/language.js`];
+
+gulp.task('compressJS', function() {
+	gulp.src(javascriptSources)
+		.pipe(uglify())
+		.pipe(concat('compressed.js'))
+		.pipe(gulp.dest('./js/'));
+})
+
+gulp.task('default')
