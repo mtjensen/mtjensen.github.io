947f78823349a1ac8625649f2aa99dab4dc03d51
mtjensen <mtyljensen@gmail.com>
2016-08-14 21:38:23 +0200

    added css compression

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,47 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
 
 var nodePath = "./node_modules";
-
-// gulp.task('vendor', function() {
-// 	gulp.src(`${nodePath}/animsition/dist/**/*`)
-// 		.pipe(gulp.dest('./vendor/animsition'))
-// 	gulp.src(`${nodePath}/fullpage.js/**/*`)
-// 		.pipe(gulp.dest('./vendor/fullpage.js'))
-// 	gulp.src(`${nodePath}/fittext.js/jquery.fittext.js`)
-// 		.pipe(gulp.dest('./vendor/fittext.js'))
-// 	gulp.src(`${nodePath}/mfb/src/**/*`)
-// 		.pipe(gulp.dest('./vendor/mfb'))
-// });
 
 var javascriptSources = [
 	`${nodePath}/bootstrap/dist/js/bootstrap.js`, 
 	`${nodePath}/fullpage.js/jquery.fullPage.js`, 
 	`${nodePath}/animsition/dist/js/animsition.js`, 
 	`${nodePath}/fittext.js/jquery.fittext.js`, 
-	`./src/classie/classie.js`,
-	`./src/cbpAnimatedHeader/cbpAnimatedHeader.js`, 
+	`./src/js/classie/classie.js`,
+	`./src/js/cbpAnimatedHeader/cbpAnimatedHeader.js`, 
 	`${nodePath}/mfb/src/mfb.js`,
-	`./src/main.js`,
-	`./src/language.js`];
+	`./src/js/main.js`,
+	`./src/js/language.js`];
+
+var cssSources = [
+	`./src/bootstrap/css/bootstrap.css`,
+	`${nodePath}/fullpage.js/jquery.fullPage.css`,
+	`${nodePath}/animsition/css/animsition.css`,
+	`${nodePath}/mfb/src/mfb.css`,
+	'src/css/main.css'
+];
+
+gulp.task('vendor', function() {
+	gulp.src(`${nodePath}/mfb/src/lib/modernizr.touch.js`)
+		.pipe(gulp.dest('./js/'))
+});
 
 gulp.task('compressJS', function() {
-	gulp.src(javascriptSources)
+	return gulp.src(javascriptSources)
 		.pipe(uglify())
 		.pipe(concat('compressed.js'))
 		.pipe(gulp.dest('./js/'));
 })
 
-gulp.task('default')
+
+gulp.task('compressCSS', function() {
+	return gulp.src(cssSources)
+		.pipe(cleanCSS())
+		.pipe(concat('compressed.css'))
+		.pipe(gulp.dest('./css/'))
+})
+
+gulp.task('default', ['vendor', 'compressJS', 'compressCSS'])
